15517c8b7b3c9d64dd91fd05c7a336d29d106a8e
Alex Barreto <axbarreto@lavabit.com>
2020-08-08 16:05:27 -0500

    Upgraded min supported TensorFlow version to 1.15 (#2169)
    Signed-off-by: Travis Addair <taddair@uber.com>

--- test/test_tensorflow.py
+++ test/test_tensorflow.py
@@ -1930,9 +1930,6 @@ class TensorFlowTests(tf.test.TestCase):
             self.assertLess(err, 0.00000001)
 
     def test_broadcast_object(self):
-        if LooseVersion(tf.__version__) < LooseVersion('1.15.0'):
-            self.skipTest("Broadcasting object requires TensorFlow 1.15 or above")
-
         hvd.init()
 
         with tf.device("/cpu:0"):
@@ -1946,9 +1943,6 @@ class TensorFlowTests(tf.test.TestCase):
             self.assertDictEqual(obj, expected_obj)
 
     def test_broadcast_object_fn(self):
-        if LooseVersion(tf.__version__) < LooseVersion('1.15.0'):
-            self.skipTest("Broadcasting object requires TensorFlow 1.15 or above")
-
         if hvd._executing_eagerly() or _IS_TF2:
             # Only for TF 1.0 in graph mode
             return
@@ -1966,10 +1960,24 @@ class TensorFlowTests(tf.test.TestCase):
             obj = bcast(obj)
             self.assertDictEqual(obj, expected_obj)
 
-    def test_elastic_state(self):
-        if LooseVersion(tf.__version__) < LooseVersion('1.15.0'):
-            self.skipTest("Broadcasting object requires TensorFlow 1.15 or above")
+    def test_allgather_object(self):
+        hvd.init()
 
+        with tf.device("/cpu:0"):
+            d = {'metric_val_1': hvd.rank()}
+            if hvd.rank() == 1:
+                d['metric_val_2'] = 42
+
+            results = hvd.allgather_object(d)
+
+            expected = [{'metric_val_1': i} for i in range(hvd.size())]
+            if hvd.size() > 1:
+                expected[1] = {'metric_val_1': 1, 'metric_val_2': 42}
+
+            self.assertEqual(len(results), hvd.size())
+            self.assertListEqual(results, expected)
+
+    def test_elastic_state(self):
         if not hvd._executing_eagerly() and _IS_TF2:
             # Only support TF 2.0 in eager mode
             return
